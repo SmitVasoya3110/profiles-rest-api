a42f6d570de8881fa21951560dfabf2c3241fdf7
smit <ssssmmmmiiiitttt@gmail.com>
2021-01-05 13:31:05 +0530

    Serializers, Permissions, Token Authentication

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import permissions
+from .models import UserProfile
 
 
 # Create your views here.
@@ -95,3 +98,10 @@ class HelloViewSet(viewsets.ViewSet):
         """Updating part of object"""
         return Response({'Http':'DELETE'})
 
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = UserProfile.objects.all()
+    authentication_calsses = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    
